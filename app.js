ea995cb74365baf70c51a354487024ce9ac43ffe
Diegolas <diegolas.code@gmail.com>
2025-03-17 14:07:50 -0300

    rehecha funcion

--- app.js
+++ app.js
@@ -2,23 +2,24 @@
 
 let amigos = [];
 
-function agregarAmigo()
-{
+function agregarAmigo() {
+    
     const input = document.getElementById("amigo");
     const nombre = input.value.trim();
 
-    if (nombre)
-    {
-        const lista = document.getElementById("listaAmigos");
-        const nuevoElemento = document.createElement("li");
-        nuevoElemento.textContent(nombre);
-        lista.append(nuevoElemento);
+    
+    if (nombre) {
+        amigos.push(nombre);
+
+        // const lista = document.getElementById("listaAmigos");
+        // const nuevoElemento = document.createElement("li");
+        // nuevoElemento.textContent = nombre;
+
+        // lista.append(nuevoElemento);
 
         input.value = "";
         input.focus();
-    }
-    else
-    {
-        alert("Ingrese un nombre válido");
+    } else {
+        alert("Por favor ingresa un nombre válido.");
     }
 }
