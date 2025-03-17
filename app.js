5fd3c3e1e58dcb74f834ec378bf62fbb8c17d1cf
Diegolas <diegolas.code@gmail.com>
2025-03-17 19:10:47 -0300

    agregar funciones actualizarLista y sortearAmigo

--- app.js
+++ app.js
@@ -2,24 +2,56 @@
 
 let amigos = [];
 
-function agregarAmigo() {
-    
+function agregarAmigo()
+{    
     const input = document.getElementById("amigo");
     const nombre = input.value.trim();
-
     
     if (nombre) {
         amigos.push(nombre);
-
-        // const lista = document.getElementById("listaAmigos");
-        // const nuevoElemento = document.createElement("li");
-        // nuevoElemento.textContent = nombre;
-
-        // lista.append(nuevoElemento);
+        actualizarLista();
 
         input.value = "";
         input.focus();
     } else {
         alert("Por favor ingresa un nombre válido.");
     }
+}
+
+function actualizarLista()
+{
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++)
+    {
+        const nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = amigos[i];
+        lista.append(nuevoElemento);
+    }
+}
+
+const inputAmigo = document.getElementById("amigo");
+
+function detectarEnter(event)
+{
+    if (event.key === "Enter") {
+        agregarAmigo();
+    }
+}
+
+inputAmigo.addEventListener("keydown", detectarEnter);
+
+function sortearAmigo()
+{
+    if (amigos.length === 0)
+    {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = `Amigo seleccionado: <strong>${amigoSorteado}</strong>`;
 }
